64c8496d7bd4c57efd1b59338c93a466bb18ac07
mhh48@mail.aub.edu <mhh48@mail.aub.edu>
2019-03-25 01:33:39 +0200

    updated login

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from .views import login_root, login_success, login_fail
 
+from django.conf.urls import url
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^login/$', login_root, name='login_root'),
+    url(r'^login/success/$', login_success, name='login_success'),
+    url(r'^login/fail/$', login_fail, name='login_fail')
+
 ]
