2434d1df587e5009d6258199a42af2ad5a60a6fb
haydar <haa156@mail.aub.edu>
2019-04-11 02:46:42 +0300

    user models

--- DoctorWebsite/urls.py
+++ DoctorWebsite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('',include('base.urls'))
 ]
--- users/views.py
+++ users/views.py
@@ -7,7 +7,14 @@ def sign_up(request):
     if request.method == 'POST':
         form=UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.address = form.cleaned_data.get('address')
+            user.profile.phone_number = form.cleaned_data.get('phone_number')
+            user.record.date_of_birth = form.cleaned_data.get('date_of_birth')
+            user.record.medical_history = form.cleaned_data.get('medical_history')
+            user.record.blood_type = form.cleaned_data.get('blood_type')
+            user.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Welcome {username}, you have successfully registered!')
             return redirect('login')
