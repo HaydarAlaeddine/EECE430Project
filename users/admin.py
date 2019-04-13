5305fe8cc1fce272a3230c27b7bce1716822913b
haydar <haa156@mail.aub.edu>
2019-04-13 03:39:34 +0300

    User able to view his profile

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile, Record
+from .models import Profile, Record,Appointment
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Record)
+admin.site.register(Appointment)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, AppointmentForm
+from .models import Appointment,Profile
+from django.contrib.auth.models import User
+
+
 
 # Create your views here.
 def sign_up(request):
@@ -16,10 +20,45 @@ def sign_up(request):
             user.record.blood_type = form.cleaned_data.get('blood_type')
             user.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Welcome {username}, you have successfully registered!')
             return redirect('login')
         else:
             messages.error(request,'Please complete all info')
     else:
         form=UserRegisterForm()
     return render(request,'users/signup.html',{'form':form})
+
+def take_appointment(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form=AppointmentForm(request.POST)
+            if form.is_valid():
+                appointment = form.save(commit=False)
+                appointments=Appointment.objects.filter(date=appointment.date)
+                if any(appointments):
+                    messages.error(request,'This time is already taken')
+                    
+                else :
+                    user = User.objects.get(username=request.user.username)
+                    appointment.user=user
+                    appointment.save()
+                    return redirect('homepage')
+                
+            else:
+                messages.error(request,'Please complete all info')
+        else:
+            form=AppointmentForm()
+        return render(request,'users/appointment.html',{'form':form})
+    else :
+        return redirect('login')
+
+def view_profile(request):
+    if request.user.is_authenticated:
+        user = User.objects.get(username=request.user.username)
+        user_profile=Profile.objects.get(user=user)
+        context = {
+            "profile":user_profile,
+            "user":user
+        }
+        return render(request,'users/profile.html',context)
+    else:
+        redirect('login')
